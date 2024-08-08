30c0b4e5ab00f3ba15c1459e60b003df46c73611
GPSLT38 <athakur@covalience.com>
2024-08-08 17:36:03 +0530

    added a utility function to rounds a number to two decimal places

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,15 @@
 export function pluck<T>(array: T[], key: keyof T): T[keyof T][] {
   return array.map((element) => element[key]);
 }
+
+
+/**
+ * The function `roundToTwoDecimalPlaces` rounds a number to two decimal places.
+ * @param {number} num - The `num` parameter in the `roundToTwoDecimalPlaces` function is a number that
+ * you want to round to two decimal places.
+ * @returns The function `roundToTwoDecimalPlaces` is returning a number that is rounded to two decimal
+ * places.
+ */
+export function roundToTwoDecimalPlaces(num: number): number {
+  return Math.round((num + Number.EPSILON) * 100) / 100;
+}
